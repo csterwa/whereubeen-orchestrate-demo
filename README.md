47d9026d3b211bb860a61489da5a1c683ece8aa7
csterwa <chris.sterling@ctl.io>
2015-07-13 16:29:18 -0700

    Added more documentation on how to start application. Removed People model. Added random-word to deployment host name for AppFog.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# whereubeen-orchestrate-demo
+# Where U Been? Orchestrate Geolocation Demo
+
 Demo application for Orchestrate and its location capabilities along with AppFog deployment.
+
+# Running Application
+
+```
+$ npm install
+$ npm start
+
+> whereubeen@1.0.0 start /Users/csterling/projects/hack/whereubeen
+> node index.js
+
+Example app listening at http://:::5000
+```
--- public/scripts/service.js
+++ public/scripts/service.js
@@ -5,12 +5,4 @@ angular.module('wubServices', ['ngResource'])
   	  save:   {method:'POST'},
       remove: {method: 'DELETE', params: {id:"@id"}}
     });
-  }])
-.factory('People', ['$resource', function($resource) {
-    return $resource('/person/:id', {}, {
-      get: {method:'GET'},
-  	  query: {method: 'GET', isArray: false},
-  	  save: {method:'POST'},
-      remove: {method: 'DELETE', params: {id:"@id"}}
-    });
   }]);
